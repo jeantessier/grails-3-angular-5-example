bdb2e833c8d33c668d76cc60f0dfcf75ef574439
Jean Tessier <jean.tessier@gusto.com>
2019-12-17 13:21:33 -0800

    Upgrade Angular 5.0.2 --> 8.2.14
    Following instructions at https://update.angular.io/#5.0:8.0
    
        $ yarn add @angular/cli@6
        $ ng update @angular/cli@6
        $ ng update @angular/core@6
        $ yarn global add rxjs-tslint
        $ rxjs-5-to-6-migrate -p src/tsconfig.app.json
        $ ng update @angular/cli@7 --force
        $ ng update @angular/core@7 --force
        $ ng update @angular/cli --force
        $ ng update @angular/core --force

--- client/src/app/nav/nav.service.ts
+++ client/src/app/nav/nav.service.ts
@@ -1,9 +1,11 @@
+
+import {refCount, publishReplay, map} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 
-import 'rxjs/add/operator/publishReplay';
-import 'rxjs/add/operator/map';
-import {Observable} from 'rxjs/Observable';
+
+
+import {Observable} from 'rxjs';
 
 import { environment } from '../../environments/environment';
 
@@ -17,10 +19,10 @@ export class NavService {
 
   getNavData(): Observable<any> {
     if (!this._navData) {
-      this._navData = this.http.get(environment.serverUrl + 'application')
-          .map((res: Response) => res.json())
-          .publishReplay()
-          .refCount();
+      this._navData = this.http.get(environment.serverUrl + 'application').pipe(
+          map((res: Response) => res.json()),
+          publishReplay(),
+          refCount(),);
     }
     return this._navData;
   }
